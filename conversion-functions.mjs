38245b5df21c882069d3fa6ebd4d6788cdd28c83
Nahum Alon <nochum.shepsl@gmail.com>
2025-01-20 16:24:17 +0200

    Added implementation of myPasreInt with different radix

--- conversion-functions.mjs
+++ conversion-functions.mjs
@@ -1,36 +1,64 @@
-export function myParseInt(strNum) {
+export function myParseInt(strNum, radix) {
     let res = NaN;
-    if (strNum != null && strNum != undefined) {
+    let actualRadix = getActualRadix(radix);
+    if (strNum != null && strNum != undefined && !isNaN(actualRadix)) {
         let i = 0;
         let sign = 1;
         strNum = strNum.toString();
         strNum = strNum.trim();
-        if (strNum[0] == "-") {
-            sign = -1;
-            i++;
-        } else if (strNum[0] == "+") {
-            i++;
-        }
-        if (i < strNum.length && !isNaN(getDigit(strNum[i]))) {
-            res = 0;
-            let running = true;
-            while (i < strNum.length && running) {
-                let digit = getDigit(strNum[i]);
-                if (isNaN(digit)) {
-                    running = false;
-                } else {
-                    res = res * 10 + digit
-                    i++;
-                }
-            }
-            res *= sign;
-        }
+        ({ sign, i } = signProcessing(strNum, sign, i));
+        res = digitsProcessing(i, strNum, res, sign, actualRadix);
     }
     return res;
 }
 
-function getDigit(digitStr) {
-    return digitStr >= "0" && digitStr <= "9" ? +digitStr : NaN
+function getActualRadix(radix) {
+    let res = NaN;
+    if (radix == undefined) {
+        res = 10;
+    } else if (radix >= 2 && radix <= 36) {
+        res = radix;
+    }
+    return res;
+}
+
+function digitsProcessing(i, strNum, res, sign, radix) {
+    if (i < strNum.length && !isNaN(getDigit(strNum[i], radix))) {
+        res = 0;
+        let running = true;
+        while (i < strNum.length && running) {
+            let digit = getDigit(strNum[i], radix);
+            if (isNaN(digit)) {
+                running = false;
+            } else {
+                res = res * radix + digit;
+                i++;
+            }
+        }
+        res *= sign;
+    }
+    return res;
+}
+
+function signProcessing(strNum, sign, i) {
+    if (strNum[0] == "-") {
+        sign = -1;
+        i++;
+    } else if (strNum[0] == "+") {
+        i++;
+    }
+    return { sign, i };
+}
+
+function getDigit(digitStr, radix) {
+    digitStr = digitStr.toLowerCase();
+    const code = digitStr.charCodeAt(0);
+    const zeroCode = "0".charCodeAt(0);
+    const nineCode = "9".charCodeAt(0);
+    const aCode = "a".charCodeAt(0);
+    let base = code > nineCode ? aCode - 10 : zeroCode;
+    let res = code - base;
+    return res >= 0 && res < radix ? res : NaN;
 }
 
 export function myToStringFromIntNumber(number) {
--- test/conversion-functions.test.mjs
+++ test/conversion-functions.test.mjs
@@ -1,6 +1,45 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, test } from "vitest";
 import { myParseInt, myToStringFromIntNumber } from "../conversion-functions.mjs";
+test ("swap primitives", () => {
+    let a = 10;
+    let b = 20;
+    [b, a] = [a, b];
+    expect(a).toBe(20);
+    expect(b).toBe(10);
+});
+describe("myPasreInt with radix test suit", () => {
+    it("A 11- > 10", () => {
+        expect(myParseInt("A", 11)).toBe(10);
+    });
+    it("aa 16 - > 170", () => {
+        expect(myParseInt("aa", 16)).toBe(170);
+    });
+    it("az 16 -> 10", () => {
+        expect(myParseInt("az", 16)).toBe(10);
+    });
+    it("A null -> Nan", () => {
+        expect(myParseInt("A", null)).toBeNaN();
+    });
+    it("A 0 -> NaN", () => {
+        expect(myParseInt("A", 0)).toBeNaN();
+    });
+    it("A -1 -> NaN", () => {
+        expect(myParseInt("A", -1)).toBeNaN();
+    });
+    it("A -1 -> NaN", () => {
+        expect(myParseInt("A", 10)).toBeNaN();
+    });
+    it("A 37 -> 10", () => {
+        expect(myParseInt("A", 37)).toBeNaN();
+    });
+    it("FZ 36 -> 10", () => {
+        expect(myParseInt("FZ", 36)).toBe(parseInt("FZ", 36));
+    });
+});
 describe("myPasreInt test suit", () => {
+    it("integer zero", () => {
+        expect(myParseInt(0)).toBe(0);
+    });
     it("integer number", () => {
         expect(myParseInt("12")).toBe(12);
     });
