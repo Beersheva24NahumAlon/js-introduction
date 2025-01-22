1e1ab6b542c30f7c712668116081f32617c23eab
Nahum Alon <nochum.shepsl@gmail.com>
2025-01-22 23:28:42 +0200

    Added function myBind and tests for it

--- functions.mjs
+++ functions.mjs
@@ -1,31 +1,34 @@
-// function sum(num1, num2) {
-//     return this.x * num1 + this.x * num2;
+// // function sum(num1, num2) {
+// //     return this.x * num1 + this.x * num2;
+// // }
+// // sum.a = 2;
+// // sum.call({x:5}, 10, 10);
+// const point = {
+//     x: 5,
+//     y:10
+// };
+// function displayPoint(z) {
+//     console.log(`x:${this.x}; y:${this.y}; z:${z}`)
 // }
-// sum.a = 2;
-// sum.call({x:5}, 10, 10);
-const point = {
-    x: 5,
-    y:10
-};
-function displayPoint(z) {
-    console.log(`x:${this.x}; y:${this.y}; z:${z}`)
-}
-const display = displayPoint.bind(point);
-display(40)
-// Arrow-functions
-const displayPointArrow = (z) => {
-    console.log(`x: ${this.x}, y: ${this.y}, z: ${z}`)
-};
-const displayArrow = displayPointArrow.bind(point, 40);
-//throws exception, because arrow-functions doesn't have "this"
-// displayArrow(); 
+// const display = displayPoint.bind(point);
+// display(40)
+// // Arrow-functions
+// const displayPointArrow = (z) => {
+//     console.log(`x: ${this.x}, y: ${this.y}, z: ${z}`)
+// };
+// const displayArrow = displayPointArrow.bind(point, 40);
+// //throws exception, because arrow-functions doesn't have "this"
+// // displayArrow(); 
 
-//In method "call" we should pass parametrs
-display.call(point, 45);
-//In method "apply" we should pass array of parametrs
-display.apply(point, [50]);
+// //In method "call" we should pass parameters
+// display.call(point, 45);
+// //In method "apply" we should pass array of parametrs
+// display.apply(point, [50]);
 
 export function myBind(thisArg) {
-    //TODO
-    //method apply
+    const func = (num3, num4) => { 
+        return this.apply(thisArg, [num3, num4]);
+    };
+    return func;
 }
+
