3d473e67f79b0b642282c81951ae96678d1a4add
Nahum Alon <nochum.shepsl@gmail.com>
2025-01-26 12:37:00 +0200

    finding elements in array

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,22 +1,44 @@
 import { describe, it, expect, test } from "vitest";
-describe("removing elements from array", () => {
-    
-    it("removing first element of array", () => {
-        const array = [1, 2, 3, 4];
-        const expected = [2, 3, 4];
-        array.shift();
-        expect(array).toEqual(expected);
+describe("finding elements in array", () => {
+    it("method indexOf / lastIndexOf for primitivies", () => {
+        const array = [1, 2, 3, 2, 4];
+        expect(array.indexOf(6)).toBe(-1);
+        expect(array.indexOf(2)).toBe(1);
+        expect(array.lastIndexOf(2)).toBe(3);
     });
-    it("removing last element of array", () => {
-        const array = [1, 2, 3, 4];
-        const expected = [1, 2, 3];
-        array.pop();
-        expect(array).toEqual(expected);
+    it("finding in array of objects", () => {
+        const obj1 = {x: 5};
+        const array = [
+            {x: 4}, 
+            obj1, 
+            {x: 6}
+        ];
+        expect(array.indexOf({x: 4})).toBe(-1); //different references
+        expect(array.indexOf(obj1)).toBe(1);
+        expect(array.findIndex(o => o.x === 4)).toBe(0);
+        expect(array.findIndex(o => o.x === 7)).toBe(-1);
     });
-    it("removing middle element of array", () => {
-        const array = [1, 2, 3, 4];
-        const expected = [1, 4];
-        array.splice(1, 2);
-        expect(array).toEqual(expected);
+    it("finding object in array of objects", () => {
+        const obj1 = {x: 5};
+        const array = [
+            {x: 4}, 
+            obj1, 
+            {x: 6}
+        ];
+        expect(array.find(o => o.x === 4)).toEqual({x: 4});
+        expect(array.find(o => o.x === 7)).toBeUndefined();
+    });
+    it("finding several primitivies", () => {
+        const array = [1, 2, 3, 2, 4];
+        expect(array.filter(n => n % 2 == 0)).toEqual([2, 2, 4]);
+    });
+    it("finding several objects", () => {
+        const obj1 = {x: 5};
+        const array = [
+            {x: 4}, 
+            obj1, 
+            {x: 7}
+        ];
+        expect(array.filter(o => o.x % 2 == 1)).toEqual([{x: 5}, {x: 7}]);
     });
 });
