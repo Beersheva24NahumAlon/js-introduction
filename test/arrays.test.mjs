bcb13143d49e5df2f7589095d6bef224e526ff74
Nahum Alon <nochum.shepsl@gmail.com>
2025-01-26 12:05:54 +0200

    removing elements from array

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,22 +1,22 @@
 import { describe, it, expect, test } from "vitest";
-describe("meaning of spread operator for arrays and arguments", () => {
-    it("finding maximal number of array", () => {
-        const numbers = [1, 2, 3, 4];
-        expect(Math.max(1, 2)).toBe(2);
-        expect(Math.max(...numbers)).toBe(4);
-    });
-    it("pushing to array", () => {
+describe("removing elements from array", () => {
+    
+    it("removing first element of array", () => {
         const array = [1, 2, 3, 4];
-        const expectedArray = [1, 2, 3, 4, 5, 6];
-        const pushArray = [5, 6];
-        array.push(...pushArray);
-        //array.push(5, 6);
-        expect(array).toEqual(expectedArray);
+        const expected = [2, 3, 4];
+        array.shift();
+        expect(array).toEqual(expected);
     });
-    it("coping arrays", () => {
-        const srcArray = [1, 2, 3, 4];
-        const destArray = [...srcArray];
-        expect(destArray).not.toBe(srcArray);
-        expect(destArray).toEqual(srcArray);
+    it("removing last element of array", () => {
+        const array = [1, 2, 3, 4];
+        const expected = [1, 2, 3];
+        array.pop();
+        expect(array).toEqual(expected);
+    });
+    it("removing middle element of array", () => {
+        const array = [1, 2, 3, 4];
+        const expected = [1, 4];
+        array.splice(1, 2);
+        expect(array).toEqual(expected);
     });
 });
