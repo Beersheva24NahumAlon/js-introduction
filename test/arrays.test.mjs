d676a6a315dfa7663e4695c7f569c75d547a7351
Nahum Alon <nochum.shepsl@gmail.com>
2025-01-26 12:54:44 +0200

    iterating elements in array

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,44 +1,23 @@
 import { describe, it, expect, test } from "vitest";
-describe("finding elements in array", () => {
-    it("method indexOf / lastIndexOf for primitivies", () => {
+describe("iterating elements in array", () => {
+    it("printing out the elements using for-each", () => {
         const array = [1, 2, 3, 2, 4];
-        expect(array.indexOf(6)).toBe(-1);
-        expect(array.indexOf(2)).toBe(1);
-        expect(array.lastIndexOf(2)).toBe(3);
+        array.forEach(element => console.log(element));
     });
-    it("finding in array of objects", () => {
-        const obj1 = {x: 5};
-        const array = [
-            {x: 4}, 
-            obj1, 
-            {x: 6}
-        ];
-        expect(array.indexOf({x: 4})).toBe(-1); //different references
-        expect(array.indexOf(obj1)).toBe(1);
-        expect(array.findIndex(o => o.x === 4)).toBe(0);
-        expect(array.findIndex(o => o.x === 7)).toBe(-1);
-    });
-    it("finding object in array of objects", () => {
-        const obj1 = {x: 5};
-        const array = [
-            {x: 4}, 
-            obj1, 
-            {x: 6}
-        ];
-        expect(array.find(o => o.x === 4)).toEqual({x: 4});
-        expect(array.find(o => o.x === 7)).toBeUndefined();
-    });
-    it("finding several primitivies", () => {
+    it("printing out the elements using for-in", () => {
         const array = [1, 2, 3, 2, 4];
-        expect(array.filter(n => n % 2 == 0)).toEqual([2, 2, 4]);
+        for (const index in array) {
+            console.log(array[index]);
+        }
     });
-    it("finding several objects", () => {
-        const obj1 = {x: 5};
-        const array = [
-            {x: 4}, 
-            obj1, 
-            {x: 7}
-        ];
-        expect(array.filter(o => o.x % 2 == 1)).toEqual([{x: 5}, {x: 7}]);
+    it("printing out the elements using for-of", () => {
+        const array = [1, 2, 3, 2, 4];
+        for (const element of array) {
+            console.log(element);
+        }
+    });
+    it("printing out indexes and elements of array", () => {
+        const array = [1, 2, 3, 2, 4];
+        array.forEach((element, index) => console.log(`${index}: ${element}`));
     });
 });
