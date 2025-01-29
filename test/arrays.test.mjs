e89274ec3662ba7ffa7dd0a5745ee72be484f024
Nahum Alon <nochum.shepsl@gmail.com>
2025-01-29 11:19:17 +0200

    destructuring of array

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,19 +1,16 @@
 import { describe, it, expect, test } from "vitest";
-import { myMap, myReduce } from "../arrays.mjs";
-describe("map and reduce methods", () => {
+describe("destructuring of array", () => {
     const array = [10, 1000, -10, 30, 60];
-    array.map = myMap;
-    array.reduce = myReduce;
-    it("myMap testing", () => {
-        const expected = [10, 1001, -8, 33, 64];
-        expect(array.map((e, i) => e + i)).toEqual(expected);
+    it("assigning two first elements of array to variables", () => {
+        const [a, b] = array;
+        expect(a).toBe(array[0]);
+        expect(b).toBe(array[1]);
     });
-    it("myReduce testing", () => {
-        expect(array.reduce((sum, e) => sum += e)).toBe(1090);
-        expect(array.reduce((obj, e) => {
-            obj.min = e < obj.min ? e : obj.min;
-            obj.max = e > obj.max ? e : obj.max;
-            return obj;
-        }, { min: array[0], max: array[0] })).toEqual({ min: -10, max: 1000 });
+    it("swaping two variables", () => {
+        let a = 1;
+        let b = 2;
+        [a, b] = [b, a];
+        expect(a).toBe(2);
+        expect(b).toBe(1);
     });
 });
