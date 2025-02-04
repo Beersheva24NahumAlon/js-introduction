c4e48fa525d5320a13737d9f1d06e22637f0eb14
Nahum Alon <nochum.shepsl@gmail.com>
2025-02-04 15:00:09 +0200

    iterator

--- test/objects.test.mjs
+++ test/objects.test.mjs
@@ -1,53 +1,32 @@
 import { describe, it, expect, test } from "vitest";
-describe("Object copying", () => {
-    it("showing example of references assignment but not copy", () => {
-        const person1 = { name: "Vasya", age: 25 };
-        const person2 = person1;
-        person2.gender = "male";
-        expect(person1.gender).toBe("male");
+import { makeIteratorClosedRange, makeIterableClosedRange } from "../objects.mjs";
+import { ac } from "vitest/dist/chunks/reporters.D7Jzd9GS.js";
+describe("iterator pattern test", () => {
+    it("iterator test", () => {
+        const iterator = makeIteratorClosedRange(1, 3);
+        const expected = [1, 2, 3];
+        const actual = []; 
+        while (true) {
+            const {value, done} = iterator.next();
+            if (done) {
+                break;
+            } 
+            actual.push(value);
+        }
+        expect(actual).toEqual(expected);
     });
-    it("copy using spread opeator", () => {
-        const person1 = { name: "Vasya", age: 25 };
-        const person2 = { ...person1 };
-        person2.city = "Lod";
-        expect(person1.city).toBeUndefined;
-        person1.city = "Lod";
-        expect(person1).not.toBe(person2);
-        expect(person1).toEqual(person2);
-    });
-    it("copy using method assign of class Object", () => {
-        const person1 = { name: "Vasya", age: 25 };
-        const person2 = Object.assign(person1);
-        person2.city = "Lod";
-        expect(person1.city).toBeUndefined;
-        person1.city = "Lod";
-        expect(person1).toBe(person2);
-        expect(person1).toEqual(person2);
-    });
-    it("Not deep copying", () => {
-        const person1 = {id: 123, address: {city: "BR7"}};
-        const person1Copy = {...person1};
-        expect(person1Copy).not.toBe(person1); 
-        expect(person1Copy).toEqual(person1);
-        person1Copy.address.city = "Ashkelon";
-        expect(person1.address.city).toBe("Ashkelon");
-    });
-    it("Deep copying", () => {
-        const person1 = {id: 123, address: {city: "BR7"}};
-        const person1Copy = JSON.parse(JSON.stringify(person1));
-        expect(person1Copy).not.toBe(person1); 
-        expect(person1Copy).toEqual(person1);
-        person1Copy.address.city = "Ashkelon";
-        expect(person1.address.city).toBe("BR7");
-    });
-});
-describe("JSON functionality", () => {
-    const obj = { id: 123, department: "QA", basicSalary: 10000 };
-    const json = '{"id":123,"department":"QA","basicSalary":10000}';
-    it("getting JSON from object", () => {
-        expect(JSON.stringify(obj)).toBe(json);
-    });
-    it("getting object from JSON", () => {
-        expect(JSON.parse(json)).toEqual(obj);
+    it("iterable test", () => {
+        const iterable = makeIterableClosedRange(1, 3);
+        const expected = [1, 2, 3];
+        const actual = [];
+        for (const num of iterable) {
+            actual.push(num);
+        }
+        expect(actual).toEqual(expected);
+        actual.length = 0;
+        for (const num of iterable) {
+            actual.push(num);
+        }
+        expect(actual).toEqual(expected);
     });
 });
